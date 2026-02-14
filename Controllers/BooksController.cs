e01190237dd0d0a5bac87b706e474fa14a0ca373
Martim Neves <168484198+ceraizi@users.noreply.github.com>
2026-02-14 14:48:11 +0000

    get book by id

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -22,6 +22,18 @@ public class BooksController : ControllerBase{
         return await _context.Books.ToListAsync();
     }
 
+    // GET: api/books/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Book>> GetBook(int id){
+        var book = await _context.Books.FindAsync(id);
+
+        if (book == null){
+            return NotFound(new {message = "Book not found"});
+        }
+
+        return book;
+    }
+
     // POST: api/books
     [HttpPost]
     public async Task<ActionResult<Book>> PostBook(CreateBookDto bookDto){
