5dba986b07f53f29083042f728c766273f3fa0dd
Martim Neves <168484198+ceraizi@users.noreply.github.com>
2026-02-14 14:30:04 +0000

    post and get implementation

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Singularity.Data;
+using Singularity.Models;
+
+namespace Singularity.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public BooksController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/books
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+    {
+        return await _context.Books.ToListAsync();
+    }
+
+    // POST: api/books
+    [HttpPost]
+    public async Task<ActionResult<Book>> PostBook(Book book)
+    {
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetBooks), new {id = book.Id}, book);
+    }
+}
